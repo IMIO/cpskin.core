ef5abfc601f15e31885c006eacedb70548699ed6
schminitz <gillian@affinitic.be>
2014-05-13 16:00:01 +0200

    No need to parse folder ##

--- cpskin/core/tests/test_robot.py
+++ cpskin/core/tests/test_robot.py
@@ -2,26 +2,14 @@
 from plone.testing import layered
 from cpskin.core.testing import CPSKIN_CORE_ROBOT_TESTING
 
-import os
 import robotsuite
 import unittest
 
 
 def test_suite():
     suite = unittest.TestSuite()
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    robot_dir = os.path.join(current_dir, 'robot')
-    robot_tests = [
-        os.path.join('robot', doc) for doc in
-        os.listdir(robot_dir) if doc.endswith('.robot') and
-        doc.startswith('test_')
-    ]
-    for robot_test in robot_tests:
-        robottestsuite = robotsuite.RobotTestSuite(robot_test)
-        suite.addTests([
-            layered(
-                robottestsuite,
-                layer=CPSKIN_CORE_ROBOT_TESTING
-            ),
-        ])
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite('robot'),
+                layer=CPSKIN_CORE_ROBOT_TESTING),
+    ])
     return suite
