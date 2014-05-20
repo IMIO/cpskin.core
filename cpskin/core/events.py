53bb88e7426ba50d5527c577aba039bcc2ea7a21
Laurent Lasudry <lasudry@gmail.com>
2014-05-20 13:38:37 +0200

    Add layer and condition on logged-in subscribers

--- cpskin/core/events.py
+++ cpskin/core/events.py
@@ -2,19 +2,27 @@ from plone import api
 
 from Products.CMFCore.utils import getToolByName
 
+from cpskin.core.interfaces import ICPSkinCoreWithMembersLayer
+
 
 def user_logged_in(event):
     """
     When a user is logged in : define came_from
+    Applies only if members-config profile has been installed
     """
     portal = api.portal.get()
+
+    request = getattr(portal, "REQUEST", None)
+    if not ICPSkinCoreWithMembersLayer.providedBy(request):
+        return
+
     pm = getToolByName(portal, 'portal_membership')
+
     # needed because happens after notifying the event we are subscribed to :
     pm.createMemberArea()
 
     if 'citizens' in pm.getAuthenticatedMember().getGroups():
         home = pm.getHomeFolder()
-        request = getattr(portal, "REQUEST", None)
         request.set('came_from', home.absolute_url())
         request.form['came_from'] = home.absolute_url()
 
@@ -24,11 +32,19 @@ def user_initial_logged_in(event):
     When a user is logged in for the first time :
       - define came_from
       - restrict content
+    Applies only if members-config profile has been installed
     """
     portal = api.portal.get()
+
+    request = getattr(portal, "REQUEST", None)
+    if not ICPSkinCoreWithMembersLayer.providedBy(request):
+        return
+
     pm = getToolByName(portal, 'portal_membership')
+
     # needed because happens after notifying the event we are subscribed to :
     pm.createMemberArea()
+
     members = pm.getMembersFolder()
     if 'citizens' in pm.getAuthenticatedMember().getGroups():
         request = getattr(portal, "REQUEST", None)
@@ -38,6 +54,7 @@ def user_initial_logged_in(event):
     home = pm.getHomeFolder()
     if home is None:
         return
+
     # Restrict content
     local_roles = members.getLocallyAllowedTypes()
     home.setConstrainTypesMode(1)
