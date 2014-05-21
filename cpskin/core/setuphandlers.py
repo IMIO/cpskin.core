1773bb03b3581fe19d9de577cf8e32d9d5ddf53c
schminitz <gillian@affinitic.be>
2014-05-21 15:05:52 +0200

    Put hidden tags index creation into an import step and not in catalog.xml anymore affinitic refs #5732

--- cpskin/core/setuphandlers.py
+++ cpskin/core/setuphandlers.py
@@ -8,6 +8,9 @@ from plone.app.controlpanel.security import ISecuritySchema
 
 logger = logging.getLogger('cpskin.core')
 
+# The profile id of your package:
+PROFILE_ID = 'profile-cpskin.core:default'
+
 
 def installCore(context):
     if context.readDataFile('cpskin.core-default.txt') is None:
@@ -108,3 +111,50 @@ def addMaildropHost(self):
         except AttributeError:
             # if MaildropHost is not available, we pass...
             pass
+
+
+def add_catalog_indexes(context):
+    """Method to add our wanted indexes to the portal_catalog.
+
+    @parameters:
+
+    When called from the import_various method below, 'context' is
+    the plone site and 'logger' is the portal_setup logger.  But
+    this method can also be used as upgrade step, in which case
+    'context' will be portal_setup and 'logger' will be None.
+
+
+    Run the catalog.xml step as that may have defined new metadata
+    columns.  We could instead add <depends name="catalog"/> to
+    the registration of our import step in zcml, but doing it in
+    code makes this method usable as upgrade step as well.  Note that
+    this silently does nothing when there is no catalog.xml, so it
+    is quite safe.
+    """
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'catalog')
+
+    catalog = getToolByName(context, 'portal_catalog')
+    indexes = catalog.indexes()
+    # Specify the indexes you want, with ('index_name', 'index_type')
+    wanted = (('hiddenTags', 'KeywordIndex'),
+              )
+    indexables = []
+    for name, meta_type in wanted:
+        if name not in indexes:
+            catalog.addIndex(name, meta_type)
+            indexables.append(name)
+            logger.info("Added %s for field %s.", meta_type, name)
+    if len(indexables) > 0:
+        logger.info("Indexing new indexes %s.", ', '.join(indexables))
+        catalog.manage_reindexIndex(ids=indexables)
+
+
+def import_various(context):
+    """Import step for configuration that is not handled in xml files.
+    """
+    # Only run step if a flag file is present
+    if context.readDataFile('cpskin.core-default.txt') is None:
+        return
+    site = context.getSite()
+    add_catalog_indexes(site)
