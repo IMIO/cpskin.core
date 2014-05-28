82ae2589225a6afb3571fe91062a479bf0379294
Benoît Suttor <benoit.suttor@imio.be>
2014-05-28 11:59:42 +0200

    move viewlet into viewlet folder

--- cpskin/core/browser/viewlets.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from plone.app.layout.viewlets import common
-
-
-class MenuToolsViewlet(common.ViewletBase):
-
-    def get_menus(context):
-        return
---
+++ cpskin/core/viewlets/menutools.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from plone.app.layout.viewlets import common
+from plone import api
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class MenuToolsViewlet(common.ViewletBase):
+
+    index = ViewPageTemplateFile('templates/menutools.pt')
+    def get_faceted(self):
+        view = {}
+        view['href'] = 'naviguer-par-facettes'
+        view['title'] = u'Naviguer par facettes'
+        return view
+
+    def get_toolbox(self):
+        view = {}
+        view['href'] = 'boite-a-outils'
+        view['title'] = u'Boite à outils'
+        return view
+
+    def get_overlay(self):
+        page = api.content.get('boite-a-outils')
+        return page.getText()
