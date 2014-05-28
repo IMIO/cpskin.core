9e6e78b44c0da4502bd004cc53d15ce58dc018ff
Benoît Suttor <benoit.suttor@imio.be>
2014-05-28 12:40:57 +0200

    cleanup code for MenuTools Viewlet

--- cpskin/core/viewlets/menutools.py
+++ cpskin/core/viewlets/menutools.py
@@ -10,16 +10,14 @@ class MenuToolsViewlet(common.ViewletBase):
     index = ViewPageTemplateFile('menutools.pt')
     def get_faceted(self):
         view = {}
-        view['href'] = 'naviguer-par-facettes'
-        view['title'] = u'Naviguer par facettes'
+        page = api.content.get('/naviguer-par-facettes')
+        view['href'] = page.absolute_url()
+        view['title'] = page.title
         return view
 
     def get_toolbox(self):
         view = {}
-        view['href'] = 'boite-a-outils'
-        view['title'] = u'Boite à outils'
-        return view
-
-    def get_overlay(self):
         page = api.content.get('/boite-a-outils/boite-a-outils')
-        return page.getText()
+        view['href'] = page.absolute_url()
+        view['title'] = page.title
+        return view
