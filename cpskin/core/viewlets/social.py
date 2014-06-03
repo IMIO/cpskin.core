1314336ede6ac8d64ab9f358926e747da6e9bfc3
schminitz <gillian@affinitic.be>
2014-06-03 17:20:00 +0200

    Get registry values affinitic refs #5790

--- cpskin/core/viewlets/social.py
+++ cpskin/core/viewlets/social.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+import operator
+
+from zope.component import getUtility
+
 from plone.app.layout.viewlets import common
+from plone.registry.interfaces import IRegistry
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -7,3 +12,9 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class SocialViewlet(common.ViewletBase):
 
     index = ViewPageTemplateFile('social.pt')
+
+    def getSocialLinks(self):
+        registry = getUtility(IRegistry)
+        links_dict = registry['cpskin.core.socialviewlet']
+        links_ordered = sorted(links_dict.itervalues(), key=operator.itemgetter(0))
+        return links_ordered
