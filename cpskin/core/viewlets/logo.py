d14209bcacf5774ddffb3dffb97db52f9fbc619c
FBruynbroeck <francois@affinitic.be>
2014-06-03 13:44:26 +0200

    Override logoviewlet refs #5789

---
+++ cpskin/core/viewlets/logo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from plone.app.layout.viewlets.common import LogoViewlet
+
+
+class CPSkinLogoViewlet(LogoViewlet):
+
+    def update(self):
+        super(CPSkinLogoViewlet, self).update()
+        logoName = 'cpskinlogo.png'
+        logoTitle = self.portal_state.portal_title()
+        try:
+            logo_custom = self.context.restrictedTraverse(logoName)
+        except AttributeError:
+            #If not custom logo
+            pass
+        else:
+            self.logo_tag = logo_custom.tag(title=logoTitle, alt=logoTitle)
