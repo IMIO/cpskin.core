67f62f43d56c70d1cc8e04f3b3d45a6cf8431425
Laurent Lasudry <lasudry@gmail.com>
2014-06-23 15:01:57 +0200

    Add action to configure index view This refs affinitic #5842

--- cpskin/core/browser/folderview.py
+++ cpskin/core/browser/folderview.py
@@ -1,9 +1,102 @@
 # -*- coding: utf-8 -*-
+
+from plone import api
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.interfaces import IFolderish
+
+from cpskin.locales import CPSkinMessageFactory as _
 
 
 class FolderView(BrowserView):
+
+    def can_configure(self):
+        """
+        Check if folderview can be configured on context
+        """
+        context = self.context
+        if not IFolderish.providedBy(context):
+            return False
+        layout = context.getLayout()
+        if layout != 'folderview':
+            return True
+        return False
+
+    def configure(self):
+        """
+        Configure folders and collections for folderview
+        """
+        context = self.context
+        existingIds = context.objectIds()
+        portalPath = api.portal.get().getPhysicalPath()
+        contextPath = '/'.join(context.getPhysicalPath()[len(portalPath)])
+        if 'a-la-une' not in existingIds:
+            folder = api.content.create(container=context,
+                                        type='Folder',
+                                        id='a-la-une',
+                                        title="À la une")
+            collection = api.content.create(container=folder,
+                                            type='Collection',
+                                            id='a-la-une',
+                                            title="À la une")
+            query = [{'i': 'HiddenTags',
+                      'o': 'plone.app.querystring.operation.selection.is',
+                      'v': 'a-la-une'},
+                      {'i': 'path',
+                      'o': 'plone.app.querystring.operation.string.path',
+                      'v': '/%s' % contextPath}]
+            collection.setQuery(query)
+            collection.setSort_on('effective')
+            collection.setSort_reversed(True)
+            collection.setLayout('folder_summary_view')
+            folder.setDefaultPage('a-la-une')
+        if 'actualites' not in existingIds:
+            folder = api.content.create(container=context,
+                                        type='Folder',
+                                        id='actualites',
+                                        title="Actualités")
+            collection = api.content.create(container=folder,
+                                            type='Collection',
+                                            id='actualites',
+                                            title="Actualités")
+            query = [{'i': 'portal_type',
+                      'o': 'plone.app.querystring.operation.selection.is',
+                      'v': ['News Item']},
+                      {'i': 'path',
+                      'o': 'plone.app.querystring.operation.string.path',
+                      'v': '/%s' % contextPath}]
+            collection.setQuery(query)
+            collection.setSort_on('effective')
+            collection.setSort_reversed(True)
+            collection.setLayout('folder_summary_view')
+            folder.setDefaultPage('actualites')
+        if 'evenements' not in existingIds:
+            folder = api.content.create(container=context,
+                                        type='Folder',
+                                        id='evenements',
+                                        title="Événements")
+            collection = api.content.create(container=folder,
+                                            type='Collection',
+                                            id='evenements',
+                                            title="Événements")
+            query = [{'i': 'portal_type',
+                      'o': 'plone.app.querystring.operation.selection.is',
+                      'v': ['Event']},
+                      {'i': 'path',
+                      'o': 'plone.app.querystring.operation.string.path',
+                      'v': '/%s' % contextPath}]
+            collection.setQuery(query)
+            collection.setSort_on('effective')
+            collection.setSort_reversed(True)
+            collection.setLayout('folder_summary_view')
+            folder.setDefaultPage('evenements')
+
+        context.setLayout('folderview')
+        api.portal.show_message(message=_(u"Vue index avec collections configurée."),
+                                request=self.request,
+                                type='info')
+        self.request.response.redirect(context.absolute_url())
+        return ''
 
     def getNews(self, navigation_root_path):
         path = '/'.join([navigation_root_path, 'actualites'])
