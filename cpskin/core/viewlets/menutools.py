081ba5d71385007a98e13f41b086e822a13ba243
Benoît Suttor <benoit.suttor@imio.be>
2014-06-25 15:01:56 +0200

    add multiple menutools

--- cpskin/core/viewlets/menutools.py
+++ cpskin/core/viewlets/menutools.py
@@ -12,25 +12,31 @@ class MenuToolsViewlet(common.ViewletBase):
 
     index = ViewPageTemplateFile('menutools.pt')
 
-
     @property
     def portal_catalog(self):
         return api.portal.get_tool(name='portal_catalog')
 
     def get_faceted(self):
-        view = {}
-        brains = self.portal_catalog(object_provides=IViewletMenuToolsFaceted.__identifier__)
+        menutools = []
+        brains = self.portal_catalog(
+            object_provides=IViewletMenuToolsFaceted.__identifier__,
+            sort_on='getObjPositionInParent',
+            review_state='published_and_hidden')
         if len(brains) > 0:
-            brain = brains[0]
-            view['href'] = brain.getURL()
-            view['title'] = brain.Title
-            return view
+            for brain in brains:
+                view = {}
+                view['href'] = brain.getURL()
+                view['title'] = brain.Title
+                menutools.append(view)
+            return menutools
         else:
             return False
 
     def get_toolbox(self):
         view = {}
-        brains = self.portal_catalog(object_provides=IViewletMenuToolsBox.__identifier__)
+        brains = self.portal_catalog(
+            object_provides=IViewletMenuToolsBox.__identifier__,
+            review_state='published_and_hidden')
         if len(brains) > 0:
             brain = brains[0]
             view['href'] = brain.getURL()
