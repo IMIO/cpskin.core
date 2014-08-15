7730a64e600b83140e42708c4945b417475d18aa
Martin Peeters <martin.peeters@affinitic.be>
2014-08-15 15:06:57 +0200

    Patch the _apply_index method on KeywordIndex to avoid UnicodeDecodeError errors with eea.facetednavigation queries refs #5883

--- cpskin/core/monkeypatches.py
+++ cpskin/core/monkeypatches.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from Products.PluginIndexes.KeywordIndex.KeywordIndex import KeywordIndex
 from plone import api
 
 
@@ -10,3 +11,12 @@ def afterMemberAdd(self, member, id, password, properties):
     # if this is an auto registration, not an administrator action
     if api.user.is_anonymous():
         api.group.add_user(groupname='citizens', user=member)
+
+
+def keyword_apply_index(self, request, resultset=None):
+    indexes = ('standardTags', 'iamTags', 'isearchTags', 'hiddenTags')
+    for key in [k for k in indexes if k in request and self.id == k]:
+        query_value = request[key]['query']
+        if isinstance(query_value, unicode):
+            request[key]['query'] = query_value.encode('utf8')
+    return super(KeywordIndex, self)._apply_index(request, resultset)
