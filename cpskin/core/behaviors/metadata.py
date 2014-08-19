fa796a90e05c530c496435ceca9378b5478de885
Benoît Suttor <benoit.suttor@imio.be>
2014-08-19 12:12:39 +0200

    add isearchtags behavior

--- cpskin/core/behaviors/metadata.py
+++ cpskin/core/behaviors/metadata.py
@@ -11,11 +11,11 @@ class IHiddenTags(model.Schema):
     model.fieldset(
         'categorization',
         label=_(u'label_schema_categorization', default=u'Categorization'),
-        fields=('hiddentags',),
+        fields=('hiddenTags',),
     )
 
-    hiddentags = schema.Tuple(
-        title=_(u'hidden_tags', default=u'Hidden tags'),
+    hiddenTags = schema.Tuple(
+        title=_(u'label_hidden_tags', default=u'Hidden tags'),
         description=_(
             u'help_hidden_tags',
             default=u'Tags are commonly used for ad-hoc organization of ' +
@@ -26,14 +26,59 @@ class IHiddenTags(model.Schema):
         missing_value=(),
     )
 
+
+class IISearchTags(model.Schema):
+    model.fieldset(
+        'categorization',
+        label=_(u'label_schema_categorization', default=u'Categorization'),
+        fields=('isearchTags',),
+    )
+
+    isearchTags = schema.Tuple(
+        title=_(u'label_isearch_tags',  default=u'I Search Tags'),
+        description=_(
+            u'help_isearch_tags',
+            default=u'I Search Tags are used for webmaster '
+                    u'organization of content.'
+        ),
+        value_type=schema.TextLine(),
+        required=False,
+        missing_value=(),
+    )
+
 alsoProvides(IHiddenTags, IFormFieldProvider)
+alsoProvides(IISearchTags, IFormFieldProvider)
 
 
 class HiddenTags(MetadataBase):
 
-    def _get_hiddentags(self):
-        return self.context.subject
+    @property
+    def hiddenTags(self):
+        return self.context.hiddenTags
 
-    def _set_hiddentags(self, value):
-        self.context.subject = value
-    hiddentags = property(_get_hiddentags, _set_hiddentags)
+    @hiddenTags.setter
+    def hiddenTags(self, value):
+        values = []
+        for index in value:
+            if isinstance(index, unicode):
+                values.append(index.encode('utf8'))
+            else:
+                values.append(index)
+        self.context.hiddenTags = tuple(values)
+
+
+class ISearchTags(MetadataBase):
+
+    @property
+    def isearchTags(self):
+        return self.context.isearchTags
+
+    @isearchTags.setter
+    def isearchTags(self, value):
+        values = []
+        for index in value:
+            if isinstance(index, unicode):
+                values.append(index.encode('utf8'))
+            else:
+                values.append(index)
+        self.context.isearchTags = tuple(values)
