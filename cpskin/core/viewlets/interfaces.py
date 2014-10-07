7380e1813cd1bd2fbaff93f74d39849bb1e75ea0
Benoît Suttor <benoit.suttor@imio.be>
2014-10-07 15:15:43 +0200

    Readd marker interfaces for migration step

---
+++ cpskin/core/viewlets/interfaces.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from zope.interface import Interface
+
+
+class IViewletMenuToolsFaceted(Interface):
+    """
+    marker interface used to viewlets menutools
+    """
+
+
+class IViewletMenuToolsBox(Interface):
+    """
+    marker interface used to viewlets menutools
+    """
