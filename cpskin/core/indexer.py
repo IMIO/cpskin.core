638789967fb07eeae5c05564ce285a01d98e73e0
Benoît Suttor <bsuttor@imio.be>
2016-03-02 15:02:04 +0100

    Add hack for error with encoding into plone formwidget

--- cpskin/core/indexer.py
+++ cpskin/core/indexer.py
@@ -11,6 +11,8 @@ except ImportError:
 
 from zope.interface import implements
 from zope.component import adapts
+import logging
+logger = logging.getLogger('cpskin.core.indexer')
 
 
 class BaseTagIndexer(object):
@@ -24,7 +26,7 @@ class BaseTagIndexer(object):
         self.catalog = catalog
 
     def _getFieldContent(self, field):
-        tags = getattr(self.context, field)
+        tags = getattr(self.context, field, None)
         if not tags:
             return ""
         encodedTags = tuple(safe_utf8(s) for s in tags)
@@ -53,3 +55,31 @@ class HiddenTagIndexer(BaseTagIndexer):
 
     def __call__(self):
         return self._getFieldContent('hiddenTags')
+
+
+from plone.app.querystring.interfaces import IQuerystringRegistryReader
+from plone.app.querystring.registryreader import QuerystringRegistryReader
+from zope.schema.interfaces import IVocabularyFactory
+from zope.component import queryUtility
+from operator import attrgetter
+from zope.i18n import translate
+from zope.i18nmessageid import Message
+from zope.component import adapts
+from plone.registry.interfaces import IRegistry
+
+
+class CpskinQuerystringRegistryReader(QuerystringRegistryReader):
+    """Adapts a registry object to parse the querystring data."""
+
+    def getVocabularyValues(self, values):
+        """Get all vocabulary values if a vocabulary is defined"""
+        values = super(CpskinQuerystringRegistryReader, self).getVocabularyValues(values)
+        cpskin_indexes = ['hiddenTags', 'isearchTags', 'iamTags']
+        values.get(self.prefix + '.field')
+        for cpskin_index in cpskin_indexes:
+            index = values.get(self.prefix + '.field').get(cpskin_index, None)
+            for value in index.values():
+                if isinstance(value, dict):
+                    i18n_value = dict((k.encode('utf8'), v) for k, v in value.items())
+                    values.get(self.prefix + '.field').get(cpskin_index)['values'] = i18n_value
+        return values
