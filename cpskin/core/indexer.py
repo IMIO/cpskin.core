3585b5f89d371dd997814cf73bc5ccfe805fe95d
Benoît Suttor <bsuttor@imio.be>
2016-03-02 15:13:53 +0100

    Remove hack for encode plone formwidget keyword, pin version to 1.1.3 fix problem

--- cpskin/core/indexer.py
+++ cpskin/core/indexer.py
@@ -55,31 +55,3 @@ class HiddenTagIndexer(BaseTagIndexer):
 
     def __call__(self):
         return self._getFieldContent('hiddenTags')
-
-
-from plone.app.querystring.interfaces import IQuerystringRegistryReader
-from plone.app.querystring.registryreader import QuerystringRegistryReader
-from zope.schema.interfaces import IVocabularyFactory
-from zope.component import queryUtility
-from operator import attrgetter
-from zope.i18n import translate
-from zope.i18nmessageid import Message
-from zope.component import adapts
-from plone.registry.interfaces import IRegistry
-
-
-class CpskinQuerystringRegistryReader(QuerystringRegistryReader):
-    """Adapts a registry object to parse the querystring data."""
-
-    def getVocabularyValues(self, values):
-        """Get all vocabulary values if a vocabulary is defined"""
-        values = super(CpskinQuerystringRegistryReader, self).getVocabularyValues(values)
-        cpskin_indexes = ['hiddenTags', 'isearchTags', 'iamTags']
-        values.get(self.prefix + '.field')
-        for cpskin_index in cpskin_indexes:
-            index = values.get(self.prefix + '.field').get(cpskin_index, None)
-            for value in index.values():
-                if isinstance(value, dict):
-                    i18n_value = dict((k.encode('utf8'), v) for k, v in value.items())
-                    values.get(self.prefix + '.field').get(cpskin_index)['values'] = i18n_value
-        return values
