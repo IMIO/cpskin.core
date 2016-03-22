2b6914d7f3f587c746084ae0683b4b5350bde5d7
Benoît Suttor <bsuttor@imio.be>
2016-03-22 14:44:03 +0100

    Add override of registryreader for cpskin tags

---
+++ cpskin/core/registryreader.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from plone.app.querystring.registryreader import QuerystringRegistryReader
+
+
+class CpskinQuerystringRegistryReader(QuerystringRegistryReader):
+    """Adapts a registry object to parse the querystring data."""
+
+    def getVocabularyValues(self, values):
+        """Get all vocabulary values if a vocabulary is defined"""
+        values = super(CpskinQuerystringRegistryReader, self).getVocabularyValues(values)
+        cpskin_indexes = ['hiddenTags', 'isearchTags', 'iamTags', 'standardTags']
+        values.get(self.prefix + '.field')
+        for cpskin_index in cpskin_indexes:
+            index = values.get(self.prefix + '.field').get(cpskin_index, None)
+            for value in index.values():
+                if isinstance(value, dict):
+                    i18n_value = dict((k.encode('utf8'), v) for k, v in value.items())
+                    values.get(self.prefix + '.field').get(cpskin_index)['values'] = i18n_value
+        return values
