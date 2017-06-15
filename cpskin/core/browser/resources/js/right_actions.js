e93577a41345cd54bbac2c721709d78419137159
Laurent Lasudry <lasudry@gmail.com>
2017-06-15 12:01:06 +0200

    Improve right actions panels (#17748)
     - Close all other panels when opening one
     - Close TOC when clicking on one of its elements

--- cpskin/core/browser/resources/js/right_actions.js
+++ cpskin/core/browser/resources/js/right_actions.js
@@ -1,12 +1,40 @@
 jQuery(document).ready(function($) {
 
-  $('#useful-links-button').click(function() {
-      $('#useful-links-container').toggleClass('hidden-right-panel');
-      $('#useful-links-container').toggleClass('shown-right-panel');
+  function hide_all() {
+      $('#right-actions-viewlet .right-panel').addClass('hidden-right-panel');
+      $('#right-actions-viewlet .right-panel').removeClass('shown-right-panel');
+  }
+
+  function toggle_useful_links(show) {
+      hide_all();
+      if (show) {
+          $('#useful-links-container').removeClass('hidden-right-panel');
+          $('#useful-links-container').addClass('shown-right-panel');
+      }
+  }
+
+  function toggle_toc(show) {
+      hide_all();
+      if (show) {
+          $('#table-of-contents-container').removeClass('hidden-right-panel');
+          $('#table-of-contents-container').addClass('shown-right-panel');
+      }
+  }
+
+  $('#useful-links-button').click(function(event) {
+      show = $('#useful-links-container').hasClass('hidden-right-panel');
+      toggle_useful_links(show);
+      event.preventDefault();
   });
-  $('#table-of-contents-button').click(function() {
-      $('#table-of-contents-container').toggleClass('hidden-right-panel');
-      $('#table-of-contents-container').toggleClass('shown-right-panel');
+
+  $('#table-of-contents-button').click(function(event) {
+      show = $('#table-of-contents-container').hasClass('hidden-right-panel');
+      toggle_toc(show);
+      event.preventDefault();
+  });
+
+  $('#document-toc a').click(function(event) {
+      hide_all();
   });
 
 });
