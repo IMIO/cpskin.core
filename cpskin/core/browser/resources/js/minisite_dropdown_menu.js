c1c2cf92aa3857464c3d62132548c34d75737ce6
Laurent Lasudry <lasudry@gmail.com>
2019-01-17 13:33:30 +0100

    Improve documentation
    This refs imio WEBOTT-9

--- cpskin/core/browser/resources/js/minisite_dropdown_menu.js
+++ cpskin/core/browser/resources/js/minisite_dropdown_menu.js
@@ -2,18 +2,16 @@ jQuery(document).ready(function($) {
 
     "use strict";
 
+    // Checks if li has sub (ul) and adds class for toggle icon - just an UI
     $('.minisite-dropdown-menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
-    //Checks if li has sub (ul) and adds class for toggle icon - just an UI
 
-    $('.minisite-dropdown-menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
-    //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
-
+    // Checks if drodown menu li elements have another level (ul) :
+    //  -> if yes, the dropdown is shown as mega-menu dropdown
     $('.minisite-dropdown-menu > ul > li > ul:has(ul)').addClass('mega-sub');
+    //  -> if no, the dropdown is shown as regular dropdown
+    $('.minisite-dropdown-menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
 
-    //Mobile menu is hidden if width is more then 616px, but normal menu is displayed
-    //Normal menu is hidden if width is below 616px, and jquery adds mobile menu
-
-    //the following hides the menu when a click is registered outside
+    // Hide the menu when a click is registered outside
     $(document).on('click', function(e){
         if($(e.target).parents('.minisite-dropdown-menu').length === 0 || $(e.target).hasClass('navTree')) {
             $('.minisite-dropdown-menu > ul').removeClass('show-on-mobile');
@@ -23,8 +21,9 @@ jQuery(document).ready(function($) {
     });
 
     $('.minisite-dropdown-menu > ul > li').click(function(e) {
-        //Top level click on a menu with children
+        // Top level click on a menu with children has no effect
         if($(e.target).parents('ul').length == 1 && $(this).children('ul').length > 0) e.preventDefault();
+
         var thisMenu = $(this).children('ul');
         var prevState = thisMenu.css('display');
         $('.minisite-dropdown-menu > ul > li > ul').fadeOut(200);
