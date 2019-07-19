ac53a66f234a42efc9b20b0bc02ede7c13821ae9
Benoit Suttor <bsuttor@imio.be>
2019-07-19 16:40:49 +0200

    Become Black compliant

--- cpskin/core/browser/folder.py
+++ cpskin/core/browser/folder.py
@@ -6,51 +6,47 @@ from plone.app.contenttypes.browser.folder import FolderView
 
 class CpskinNavigationView(FolderView):
 
+class CpskinNavigationView(FolderView):
     def menus(self):
-        portal_catalog = api.portal.get_tool('portal_catalog')
-        context_path = '/'.join(self.context.getPhysicalPath())
+        portal_catalog = api.portal.get_tool("portal_catalog")
+        context_path = "/".join(self.context.getPhysicalPath())
         query = {}
-        query['review_state'] = 'published_and_shown'
-        query['path'] = {'query': context_path, 'depth': 1}
-        query['sort_on'] = 'getObjPositionInParent'
+        query["review_state"] = "published_and_shown"
+        query["path"] = {"query": context_path, "depth": 1}
+        query["sort_on"] = "getObjPositionInParent"
         return portal_catalog(query)
 
     def accesses(self):
         if self.level() < 2:
             return []
-        portal_catalog = api.portal.get_tool('portal_catalog')
-        context_path = '/'.join(self.context.getPhysicalPath())
+        portal_catalog = api.portal.get_tool("portal_catalog")
+        context_path = "/".join(self.context.getPhysicalPath())
         query = {}
-        query['path'] = {'query': context_path, 'depth': 10}
-        query['sort_on'] = 'sortable_title'
-        query['object_provides'] = 'cpskin.menu.interfaces.IDirectAccess'
+        query["path"] = {"query": context_path, "depth": 10}
+        query["sort_on"] = "sortable_title"
+        query["object_provides"] = "cpskin.menu.interfaces.IDirectAccess"
         return portal_catalog(query)
 
     def level(self):
         navroot_level = len(
-            api.portal.get_navigation_root(self.context).getPhysicalPath())
+            api.portal.get_navigation_root(self.context).getPhysicalPath()
+        )
         context_level = len(self.context.getPhysicalPath())
         return context_level - navroot_level
 
 
 class CpskinNavigationViewWithLeadImage(FolderView):
-
     def menus(self):
-        portal_catalog = api.portal.get_tool('portal_catalog')
-        context_path = '/'.join(self.context.getPhysicalPath())
+        portal_catalog = api.portal.get_tool("portal_catalog")
+        context_path = "/".join(self.context.getPhysicalPath())
         query = {}
-        query['review_state'] = 'published_and_shown'
-        query['path'] = {'query': context_path, 'depth': 1}
-        query['sort_on'] = 'getObjPositionInParent'
+        query["review_state"] = "published_and_shown"
+        query["path"] = {"query": context_path, "depth": 1}
+        query["sort_on"] = "getObjPositionInParent"
         return portal_catalog(query)
 
     def image(self, brain):
         obj = brain.getObject()
-        image = image_scale(
-            obj,
-            'leadimage-navigation',
-            'thumb',
-            generate_tag=False,
-        )
+        image = image_scale(obj, "leadimage-navigation", "thumb", generate_tag=False)
         if image:
             return image.url
