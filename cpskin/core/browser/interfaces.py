1ca2dfd10ffb1efc62064530f408a4cdfe902d76
Christophe Boulanger <christophe.boulanger@imio.be>
2020-08-06 13:44:58 +0200

    blacked file

--- cpskin/core/browser/interfaces.py
+++ cpskin/core/browser/interfaces.py
@@ -9,33 +9,25 @@ class ISendToManagerForm(Interface):
     """ Interface for describing the 'sendtomanager' form """
 
     send_from_address = EmailLine(
-        title=_(u'label_send_from',
-                default=u'From'),
-        description=_(u'help_send_from',
-                      default=u'Your email address.'),
-        required=True
+        title=_(u"label_send_from", default=u"From"),
+        description=_(u"help_send_from", default=u"Your email address."),
+        required=True,
     )
 
     comment = schema.Text(
-        title=_(u'label_comment',
-                default=u'Change request'),
-        description=_(u'help_comment',
-                      default=u'Report an error or missing information.'),
-        required=False
+        title=_(u"label_comment", default=u"Change request"),
+        description=_(
+            u"help_comment", default=u"Report an error or missing information."
+        ),
+        required=False,
     )
 
 
 class IBannerActivationView(Interface):
     """ Banner activation """
 
-    can_enable_banner = schema.Bool(
-        u'Can enable banner',
-        readonly=True
-    )
-    can_disable_banner = schema.Bool(
-        u'Can disable banner',
-        readonly=True
-    )
+    can_enable_banner = schema.Bool(u"Can enable banner", readonly=True)
+    can_disable_banner = schema.Bool(u"Can disable banner", readonly=True)
 
     def enable_banner():
         """ Enable banner
@@ -45,14 +37,8 @@ class IBannerActivationView(Interface):
         """ Disable banner
         """
 
-    can_enable_local_banner = schema.Bool(
-        u'Can enable local banner',
-        readonly=True
-    )
-    can_disable_local_banner = schema.Bool(
-        u'Can disable local banner',
-        readonly=True
-    )
+    can_enable_local_banner = schema.Bool(u"Can enable local banner", readonly=True)
+    can_disable_local_banner = schema.Bool(u"Can disable local banner", readonly=True)
 
     def enable_local_banner():
         """ Enable local banner
@@ -66,14 +52,8 @@ class IBannerActivationView(Interface):
 class IMediaActivationView(Interface):
     """ media activation """
 
-    can_enable_media = schema.Bool(
-        u'Can enable multimedia viewlet',
-        readonly=True
-    )
-    can_disable_media = schema.Bool(
-        u'Can disable multimedia viewlet',
-        readonly=True
-    )
+    can_enable_media = schema.Bool(u"Can enable multimedia viewlet", readonly=True)
+    can_disable_media = schema.Bool(u"Can disable multimedia viewlet", readonly=True)
 
     def enable_media():
         """ Enable multimedia viewlet
@@ -88,12 +68,10 @@ class INavigationToggleView(Interface):
     """ navigation toggle activation """
 
     can_enable_navigation_toggle = schema.Bool(
-        u'Can enable navigation toggle on folder',
-        readonly=True
+        u"Can enable navigation toggle on folder", readonly=True
     )
     can_disable_navigation_toggle = schema.Bool(
-        u'Can disable navigation toggle on folder',
-        readonly=True
+        u"Can disable navigation toggle on folder", readonly=True
     )
 
     def enable_navigation_toggle():
