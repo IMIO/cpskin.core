de1cbd84b41c8d81012f53b44952bd3950e1ed2a
Christophe Boulanger <christophe.boulanger@imio.be>
2021-01-06 13:44:49 +0100

    [WEB-3423] : Add new method to get state (enable/disable) for accessiblity link in foooter

--- cpskin/core/browser/utils.py
+++ cpskin/core/browser/utils.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from collective.anysurfer.interfaces import ILayerSpecific
+from collective.folderishtypes.interfaces import IFolderishType
+from cpskin.core.interfaces import ICPSkinSettings
 from Acquisition import aq_parent
+from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.utils import isDefaultPage
 from Products.Five import BrowserView
-from collective.folderishtypes.interfaces import IFolderishType
+from zope.component import getUtility
 from plone import api
 
 
@@ -14,6 +18,32 @@ class UtilsView(BrowserView):
             "imio.gdpr.interfaces.IGDPRSettings.is_text_ready", default=False
         )
 
+    def has_enabled_accessibilty_link_in_footer(self):
+        return api.portal.get_registry_record(
+            "cpskin.core.interfaces.ICPSkinSettings.enable_accessibility_link_in_footer",
+            default=False,
+        )
+
+    def edit_anysurfer_settings(self):
+        if api.user.is_anonymous():
+            return False
+        current_user = api.user.get_current()
+        roles = api.user.get_roles(username=current_user.getUserName())
+        if "Manager" not in roles and "Site Administrator" not in roles:
+            return False
+        if not ILayerSpecific.providedBy(self.request):
+            return False
+        if bool(self.request.get("change_link_visibility", False)) is True:
+            registry = getUtility(IRegistry)
+            records = registry.records
+            # cpskin.core.interfaces.ICPSkinSettings
+            value = not records["cpskin.core.interfaces.ICPSkinSettings.enable_accessibility_link_in_footer"].value
+            api.portal.set_registry_record(
+                "enable_accessibility_link_in_footer", value, interface=ICPSkinSettings
+            )
+            self.request.response.redirect(self.context.absolute_url())
+            self.request.response.setHeader('Cache-Control', 'no-cache, no-store')
+        return True
 
     def has_folderish_default(self):
         if not IFolderishType.providedBy(self.context):
