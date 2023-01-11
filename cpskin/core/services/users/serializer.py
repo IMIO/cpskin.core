579dd77ae759f1834a908dee68999f5fb3d5ad0b
Christophe Boulanger <christophe.boulanger@imio.be>
2023-01-11 15:14:59 +0100

    Remove useless code

--- cpskin/core/services/users/serializer.py
+++ cpskin/core/services/users/serializer.py
@@ -14,9 +14,7 @@ class UsersSerializer(SerializeUserToJson):
     """ Add last_login_time, login_time when getting restapi users """
 
     def __call__(self):
-        self.context.getProperty("last_login_time")
         data = super(UsersSerializer, self).__call__()
-        json_compatible(self.context.getProperty("last_login_time"))
         data["last_login_time"] = json_compatible(self.context.getProperty("last_login_time"))
         data["login_time"] = json_compatible(self.context.getProperty("login_time"))
         return data
